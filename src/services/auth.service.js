2ebd694ea3a1a221585e30f10622dfb902aba3be
PhanHuynhMy <myb2000142@student.ctu.edu.vn>
2024-06-24 08:39:14 +0700

    otp

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,56 +1,61 @@
-  import createApiClient from "./api.service";
+import createApiClient from "./api.service";
 
-  class AuthService {
-    constructor(baseUrl = "/api/v1/auth") {
-      this.api = createApiClient(baseUrl);
-    }
+class AuthService {
+  constructor(baseUrl = "/api/v1/auth") {
+    this.api = createApiClient(baseUrl);
+  }
 
-    async register(user) {
-      try {
-        const response = await this.api.post("/register", user);
-        return response.data;
-      } catch (error) {
-        this.handleError(error);
-        throw error; // Rethrow lỗi để cho phép xử lý lỗi ở phía gọi hàm
-      }
-    }
-
-    async login(user) {
-      try {
-        const response = await this.api.post("/login", user);
-        console.log("Login response data:", response.data); // Thêm logging ở đây nếu cần thiết
-        return response;
-      } catch (error) {
-        this.handleError(error);
-        throw error; // Rethrow lỗi để cho phép xử lý lỗi ở phía gọi hàm
-      }
-    }
-
-    async sendOtp(user) {
-      try {
-        const response = await this.api.post("/otp", user);
-        return response.data;
-      } catch (error) {
-        this.handleError(error);
-        throw error; // Rethrow lỗi để cho phép xử lý lỗi ở phía gọi hàm
-      }
-    }
-
-    handleError(error) {
-      if (error.response) {
-        // Lỗi phản hồi từ server (ví dụ: lỗi 400, 500, ...)
-        console.error("Response error:", error.response.data);
-        console.error("Response status:", error.response.status);
-        console.error("Response headers:", error.response.headers);
-      } else if (error.request) {
-        // Yêu cầu đã gửi đi nhưng không có phản hồi từ server
-        console.error("Request error:", error.request);
-      } else {
-        // Lỗi xảy ra khi thiết lập yêu cầu (ví dụ: lỗi mạng)
-        console.error("Error message:", error.message);
-      }
-      console.error("Config:", error.config);
+  async register(user) {
+    try {
+      const response = await this.api.post("/register", user);
+      return response.data;
+    } catch (error) {
+      this.handleError(error);
+      throw error;
     }
   }
 
-  export default new AuthService();
+  async login(user) {
+    try {
+      const response = await this.api.post("/login", user);
+      console.log("Login response data:", response.data);
+      return response;
+    } catch (error) {
+      this.handleError(error);
+      throw error;
+    }
+  }
+
+  async activeOtp(email, code) {
+    try {
+      // Đảm bảo rằng bạn gửi một đối tượng với các trường email và otp
+      const response = await this.api.post("/active", { email: email, code: code });
+      // Kiểm tra xem response.data có tồn tại và có thuộc tính 'message' không
+      if (response.data && response.data.message) {
+        return response.data.message;
+      } else {
+        throw new Error('Invalid response structure');
+      }
+    } catch (error) {
+      this.handleError(error);
+      throw error;
+    }
+  }
+
+
+
+  handleError(error) {
+    if (error.response) {
+      console.error("Response error:", error.response.data);
+      console.error("Response status:", error.response.status);
+      console.error("Response headers:", error.response.headers);
+    } else if (error.request) {
+      console.error("Request error:", error.request);
+    } else {
+      console.error("Error message:", error.message);
+    }
+    console.error("Config:", error.config);
+  }
+}
+
+export default new AuthService();
